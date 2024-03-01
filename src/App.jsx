44b2eeae02007eec0bbaf693be5580630a04a9d2
Ham Petrosyan <hampetrosyan007@gmail.com>
2024-03-01 19:22:34 +0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Greeting from './helloWorld'
+
+function App() {
+
+  return (
+    <>
+      <Greeting />
+    </>
+  )
+}
+
+export default App
---
+++ src/helloWorld.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+class Greeting extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello, World!
+            </div>
+        )
+    }
+}
+
+export default Greeting
